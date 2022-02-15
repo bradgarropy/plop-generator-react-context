2376e8d5f0857949106044fb573b32fcf8c0f277
Brad Garropy <bradgarropy@gmail.com>
2022-02-14 23:12:00 -0600

    separate context hook.

--- src/context.ts
+++ src/context.ts
@@ -11,12 +11,32 @@ const context = {
         {
             type: "append",
             path: "src/context/index.ts",
-            templateFile: "./templates/index.hbs",
+            templateFile: "./templates/context/index.hbs",
         },
         {
             type: "add",
             path: "src/context/{{pascalCase name}}.tsx",
-            templateFile: "./templates/context.hbs",
+            templateFile: "./templates/context/context.hbs",
+        },
+        {
+            type: "append",
+            path: "src/hooks/index.ts",
+            templateFile: "./templates/hooks/index.hbs",
+        },
+        {
+            type: "add",
+            path: "src/hooks/use{{pascalCase name}}/index.ts",
+            templateFile: "./templates/hooks/hook/index.hbs",
+        },
+        {
+            type: "add",
+            path: "src/hooks/use{{pascalCase name}}/use{{pascalCase name}}.ts",
+            templateFile: "./templates/hooks/hook/hook.hbs",
+        },
+        {
+            type: "add",
+            path: "src/hooks/use{{pascalCase name}}/use{{pascalCase name}}.test.ts",
+            templateFile: "./templates/hooks/hook/hook.test.hbs",
         },
     ],
 }
